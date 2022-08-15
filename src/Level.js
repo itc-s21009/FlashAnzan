7254bd5544fa8a5953fce43cc31ca5fe9cdd17a6
s21015 <s21015@std.it-college.ac.jp>
2022-08-15 12:46:38 +0900

    文字サイズを調整しました。

--- src/Level.js
+++ src/Level.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 export const Level = () => {
-    return (
+  return (
       <div>
         <h1 className="box3">難しさを選んでね</h1>
           <div className="box4">
-            <h2 className="size">かんたん</h2>
-            <h3 className="size">ふつう</h3>
-            <h4 className="size">むずかしい</h4>
+            <Link to="/easy">
+              <h2 className="size">かんたん</h2>
+            </Link>
+            <Link to="/nomal">
+              <h3 className="size">ふつう</h3>
+            </Link>
+            <Link to="/hard">
+              <h4 className="size">むずかしい</h4>
+            </Link>
           </div>
       </div>
     );
