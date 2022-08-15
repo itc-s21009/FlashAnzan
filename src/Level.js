489f107b8c5176f87d0b17d831589396223867a2
s21015 <s21015@std.it-college.ac.jp>
2022-08-15 12:23:16 +0900

    難易度選択画面のデザインを修正しました。

--- src/Level.js
+++ src/Level.js
@@ -2,11 +2,11 @@ export const Level = () => {
     return (
       <div>
         <h1 className="box3">難しさを選んでね</h1>
-        <div className="box4">
-        <h2>かんたん</h2>
-        <h3>ふつう</h3>
-        <h4>むずかしい</h4>
-        </div>
+          <div className="box4">
+            <h2 className="size">かんたん</h2>
+            <h3 className="size">ふつう</h3>
+            <h4 className="size">むずかしい</h4>
+          </div>
       </div>
     );
   };
