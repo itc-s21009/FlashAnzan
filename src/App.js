f77cf946e1c50ce952a65e6a646138348b898b28
s21015 <s21015@std.it-college.ac.jp>
2022-08-15 11:56:16 +0900

    難易度選択画面のデザインを作りました。

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./Home";
-import { Page1 } from "./Page1";
+import { Level } from "./Level";
 import { Page2 } from "./Page2";
 import { Page3 } from "./Page3";
 import "./App.css";
@@ -13,7 +13,7 @@ function App() {
       <div className="App">
         <Link to="/">Home</Link>
         <br />
-        <Link to="/page1">Page1</Link>
+        <Link to="/level">Level</Link>
         <br />
         <Link to="/page2">Page2</Link>
         <br />
@@ -26,8 +26,8 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/page1">
-            <Page1 />
+          <Route path="/level">
+            <Level />
           </Route>
           <Route path="/page2">
             <Page2 />
